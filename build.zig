94c309e4246afb4c58b15893322482b8a9aae172
pink-lemons <pinklemonscode@gmail.com>
2025-03-25 22:22:58 -0400

    build basics

---
+++ build.zig
@@ -0,0 +1,43 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib_mod = b.createModule(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "comptime_interfaces",
+        .root_module = lib_mod,
+    });
+
+    b.installArtifact(lib);
+
+    // Creates a step for unit testing. This only builds the test executable
+    // but does not run it.
+    const lib_unit_tests = b.addTest(.{
+        .root_module = lib_mod,
+    });
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+
+    const lib_check = b.addLibrary(.{
+        .linkage = .static,
+        .name = "comptime_interfaces",
+        .root_module = lib_mod,
+    });
+
+    const check_step = b.step("check", "Check compilation");
+    check_step.dependOn(&lib_check.step);
+}
